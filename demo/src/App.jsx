c385461b6b8ddff9627740f0b90305c45d1fed51
Shahid Niazi <102804716+ShahidAbbasKhan@users.noreply.github.com>
2023-09-20 15:27:11 +0500

    form with useState added

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import Education from "./Education";
 import PersonalInfo from "./Info";
 import Card from "./cards/Card";
@@ -10,10 +11,23 @@ function App() {
   const roll = "E-1256";
   const dataList = [
     { tech: "Blockchain", techCode: "123" },
-
     { tech: "React Js", techCode: "323" },
     { tech: "Next Js", techCode: "444" },
   ];
+
+  const [tech, setTech] = useState("");
+  const [techCode, setTechCode] = useState("");
+  const [cards, setCards] = useState([]);
+
+  const handleStates = (e) => {
+    e.preventDefault();
+    // Create a new card object and add it to the cards state
+    const newCard = { tech, techCode };
+    setCards([...cards, newCard]);
+    // Clear the input fields
+    setTech("");
+    setTechCode("");
+  };
 
   return (
     <>
@@ -22,11 +36,34 @@ function App() {
       <h2> Education Component</h2>
       <Education userClass={cls} userRollNo={roll} />
       <h2> Cards:</h2>
-      {dataList.map((card) => {
-        return (
-          <Card key={card.techCode} tech={card.tech} techCode={card.techCode} />
-        );
-      })}
+      {dataList.map((card) => (
+        <Card key={card.techCode} tech={card.tech} techCode={card.techCode} />
+      ))}
+      <br />
+      <form onSubmit={handleStates}>
+        <label>
+          Tech :
+          <input
+            type="text"
+            value={tech} // Use the state value
+            onChange={(e) => setTech(e.target.value)}
+          />
+        </label>
+        <label>
+          TechCode :
+          <input
+            type="text"
+            value={techCode} // Use the state value
+            onChange={(e) => setTechCode(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {/* Render the newly added cards */}
+      {cards.map((card, index) => (
+        <Card key={index} tech={card.tech} techCode={card.techCode} />
+      ))}
     </>
   );
 }
