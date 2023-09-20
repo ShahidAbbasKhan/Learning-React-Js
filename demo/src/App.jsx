78a137a2c47d734289fd9d309789c8105be01c30
Shahid Niazi <102804716+ShahidAbbasKhan@users.noreply.github.com>
2023-09-20 11:18:56 +0500

    multiple  values in props

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -3,10 +3,11 @@ import PersonalInfo from "./Info";
 
 function App() {
   const name = "Shahid";
+  const cnic = "38302-8345-5";
   return (
     <>
       <h2> This is Main App</h2>
-      <PersonalInfo name={name} />
+      <PersonalInfo name={name} cnic={cnic} />
     </>
   );
 }
--- demo/src/Info.jsx
+++ demo/src/Info.jsx
@@ -1,8 +1,9 @@
 const PersonalInfo = (props) => {
+  console.log({ props }); // will be shown whole obj
   return (
     <>
       <h2> Name: {props.name}</h2>
-      <h2> CNIC:</h2>
+      <h2> CNIC: {props.cnic}</h2>
     </>
   );
 };
