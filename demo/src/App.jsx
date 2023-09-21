72a86380cdc1d543abee9e6d00fc4c5f4aaf2b6c
Shahid Niazi <102804716+ShahidAbbasKhan@users.noreply.github.com>
2023-09-21 16:57:40 +0500

    useEffect hook added

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Education from "./Education";
 import PersonalInfo from "./Info";
 import Card from "./cards/Card";
@@ -19,6 +19,22 @@ function App() {
   const [tech, setTech] = useState("");
   const [techCode, setTechCode] = useState("");
   const [cards, setCards] = useState([]);
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://randomuser.me/api");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setUser(JSON.stringify(data)); // Convert into string Format
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []); // [dependecies] like on which varible useEffect will be rendered (prevent from infinite rendering)
 
   const handleStates = (e) => {
     e.preventDefault();
@@ -42,32 +58,39 @@ function App() {
       ))} */}
 
       <hr />
-      <form onSubmit={handleStates}>
-        <label>
-          Tech :
-          <input
-            type="text"
-            value={tech} // Use the state value
-            onChange={(e) => setTech(e.target.value)}
-          />
-        </label>
-        <label>
-          TechCode :
-          <input
-            type="text"
-            value={techCode} // Use the state value
-            onChange={(e) => setTechCode(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      {/* Render the newly added cards */}
-      {cards.map((card, index) => (
-        <Card key={index} tech={card.tech} techCode={card.techCode} />
-      ))}
+      <div>
+        <form onSubmit={handleStates}>
+          <label>
+            Tech :
+            <input
+              type="text"
+              value={tech} // Use the state value
+              onChange={(e) => setTech(e.target.value)}
+            />
+          </label>
+          <label>
+            TechCode :
+            <input
+              type="text"
+              value={techCode} // Use the state value
+              onChange={(e) => setTechCode(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+        {/* Render the newly added cards */}
+        {cards.map((card, index) => (
+          <Card key={index} tech={card.tech} techCode={card.techCode} />
+        ))}
+      </div>
       <hr />
       <ParentComponent />
+      <hr />
+      <div>
+        <h5>useEffect</h5>
+        <p>{user}</p>
+      </div>
     </>
   );
 }
